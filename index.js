f764b24938311b4620db3afa5215a7d07fe7e19c
younhee song <thd0563@gmail.com>
2022-05-24 22:40:19 +0900

    Feat: express & mongoose

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://younheesong:gb7HvzR29MFDebgo@basic.zrmmot3.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("connect well"))
+  .catch((err) => console.log(err));
+//
+// mongodb+srv://younheesong:<password>@basic.zrmmot3.mongodb.net/?retryWrites=true&w=majority
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
