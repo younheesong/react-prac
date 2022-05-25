2b35b25c125344ea1c626c9513579c79941ffaa1
younhee song <thd0563@gmail.com>
2022-05-25 21:35:31 +0900

    feat: body-parser

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
+//application.x-www0form-! 분석가능하게 해줌
+app.use(bodyParser.urlencoded({ extended: true }));
+//json 파일 분석 하게 해줌
+app.use(bodyParser.json());
+
 mongoose
   .connect(
     "mongodb+srv://younheesong:gb7HvzR29MFDebgo@basic.zrmmot3.mongodb.net/?retryWrites=true&w=majority"
@@ -13,6 +20,16 @@ mongoose
 // mongodb+srv://younheesong:<password>@basic.zrmmot3.mongodb.net/?retryWrites=true&w=majority
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.post("/register", (req, res) => {
+  // register 시, 필요한 정보를 client 받으면 db에 저장
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
