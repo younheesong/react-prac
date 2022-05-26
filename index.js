57eb2910e8d9588e462eebc84bd5011db14c2c97
younhee song <thd6517@naver.com>
2022-05-26 21:24:54 +0900

    feate: login with token

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const { User } = require("./models/User");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const config = require("./config/key");
-
+const cookieParser = require("cookie-parser");
 //application.x-www0form-! 분석가능하게 해 줌
 app.use(bodyParser.urlencoded({ extended: true }));
 //json 파일 분석 하게 해줌
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 mongoose
   .connect(config.mongoURI)
   .then(() => console.log("connect well"))
@@ -24,9 +24,44 @@ app.post("/register", (req, res) => {
   // register 시, 필요한 정보를 client 받으면 db에 저장
   const user = new User(req.body);
   user.save((err, userInfo) => {
+    console.log(userInfo);
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+app.post("/login", (req, res) => {
+  // 요청된 이메일을 db에서 있는지 체크
+  // 이메일이 있으면, 비밀번호 동일 체크
+  //비밀번호 동일시 token 생성
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        msg: "not find user email",
+      });
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          msg: "not match user password",
+        });
+      }
+
+      user.generateToken((err, user) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+        // token을 response에 보냄
+        return res.cookie("x_auth", user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
+        });
+      });
     });
   });
 });
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10; //salt 가 몇글자인지
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -38,7 +39,7 @@ userSchema.pre("save", function (next) {
   // password 를 암호화 시키기
   // 1. salt 생성
   // 2. salt를 이용해 암호화 하기
-  const user = this; // this는 save 시킬 data
+  let user = this; // this는 save 시킬 data
 
   if (user.isModified("password")) {
     //password가 변경될때만 아래 암호화 실행
@@ -48,13 +49,38 @@ userSchema.pre("save", function (next) {
       bcrypt.hash(user.password, salt, function (err, hash) {
         // Store hash in your password DB.
         if (err) return next(err);
+        console.log(hash);
         user.password = hash; // 기존 순순 password가 아닌 암호화된 password 를 넣어둠.
         next();
       });
     });
+  } else {
+    next();
   }
-  next();
 });
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // plainPassword 와 db에 있는 암호화된 비밀번호를 비교
+  const user = this;
+  console.log(plainPassword, this.password);
+  bcrypt.compare(plainPassword, user.password, function (err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  //jsonwebtoken 이용해서 token 생성
+  const user = this;
+  user.token = jwt.sign(user._id, "secretToken"); //user_id와 'secretToken'을 통해 토큰 만듦
+
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
